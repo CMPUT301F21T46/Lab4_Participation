2b390b7dd061f50ee2e191fac43dc60d0d6d2d95
Julian Gallego Franco <56407915+JulianCamiloGallego@users.noreply.github.com>
2021-10-04 10:20:56 -0600

    Adding Step 3

--- app/src/main/java/com/example/lab4participation/Shape.java
+++ app/src/main/java/com/example/lab4participation/Shape.java
@@ -3,7 +3,9 @@ package com.example.lab4participation;
 public class Shape {
     int x;
     int y;
-
+    
+    String color = "Baby Blue";
+    
     Shape(int x, int y){
         this.x = x;
         this.y = y;
