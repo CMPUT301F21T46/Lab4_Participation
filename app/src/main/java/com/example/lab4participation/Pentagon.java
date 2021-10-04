d671e35afe2183be9535b7f8eb1c0a7d63ff302a
Zhekai Zhao <77252538+zhekai-zhao@users.noreply.github.com>
2021-10-04 13:06:42 -0600

    p4

--- app/src/main/java/com/example/lab4participation/Pentagon.java
+++ app/src/main/java/com/example/lab4participation/Pentagon.java
@@ -3,6 +3,6 @@ package com.example.lab4participation;
 public class Pentagon extends Shape{
     Pentagon(int x, int y){
         super(x,y);
-        String color = "blue";
     }
+    String color = "blue";
 }
